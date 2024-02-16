57182e15eb2e0210d57342155adac1c9dc3b35f8
Tarmo <liivamagi.tarmo@gmail.com>
2024-02-16 13:59:33 +0200

    tehtud, ainult pildid vaja lisada

---
+++ Liivamägi_OOP_KT3/Liivamägi_OOP_KT3/Täring.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Liivamägi_OOP_KT3
+{
+    partial class Täring
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TL_startBtn = new System.Windows.Forms.Button();
+            this.TL_btnP1 = new System.Windows.Forms.Button();
+            this.TL_btnP2 = new System.Windows.Forms.Button();
+            this.TL_lblTulemus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TL_lblJukuTaring1 = new System.Windows.Forms.Label();
+            this.TL_lblJukuTaring2 = new System.Windows.Forms.Label();
+            this.TL_lblPeeterTaring2 = new System.Windows.Forms.Label();
+            this.TL_lblPeeterTaring1 = new System.Windows.Forms.Label();
+            this.TL_lblJukuTulemus = new System.Windows.Forms.Label();
+            this.TL_lblPeeterTulemus = new System.Windows.Forms.Label();
+            this.Tl_lblJukuPunktid = new System.Windows.Forms.Label();
+            this.Tl_lblPeeterPunktid = new System.Windows.Forms.Label();
+            this.TL_lblP1 = new System.Windows.Forms.Label();
+            this.TL_lblP2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TL_startBtn
+            // 
+            this.TL_startBtn.BackColor = System.Drawing.Color.Teal;
+            this.TL_startBtn.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_startBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_startBtn.Location = new System.Drawing.Point(351, 578);
+            this.TL_startBtn.Name = "TL_startBtn";
+            this.TL_startBtn.Size = new System.Drawing.Size(184, 59);
+            this.TL_startBtn.TabIndex = 0;
+            this.TL_startBtn.Text = "Alusta uut mängu";
+            this.TL_startBtn.UseVisualStyleBackColor = false;
+            this.TL_startBtn.Click += new System.EventHandler(this.TL_startBtn_Click);
+            // 
+            // TL_btnP1
+            // 
+            this.TL_btnP1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_btnP1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_btnP1.ForeColor = System.Drawing.Color.Teal;
+            this.TL_btnP1.Location = new System.Drawing.Point(141, 511);
+            this.TL_btnP1.Name = "TL_btnP1";
+            this.TL_btnP1.Size = new System.Drawing.Size(184, 59);
+            this.TL_btnP1.TabIndex = 1;
+            this.TL_btnP1.Text = "Mängib Juku";
+            this.TL_btnP1.UseVisualStyleBackColor = false;
+            this.TL_btnP1.Click += new System.EventHandler(this.TL_btnP1_Click);
+            // 
+            // TL_btnP2
+            // 
+            this.TL_btnP2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_btnP2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_btnP2.ForeColor = System.Drawing.Color.Teal;
+            this.TL_btnP2.Location = new System.Drawing.Point(560, 511);
+            this.TL_btnP2.Name = "TL_btnP2";
+            this.TL_btnP2.Size = new System.Drawing.Size(184, 59);
+            this.TL_btnP2.TabIndex = 2;
+            this.TL_btnP2.Text = "Mängib Peeter";
+            this.TL_btnP2.UseVisualStyleBackColor = false;
+            this.TL_btnP2.Click += new System.EventHandler(this.TL_btnP2_Click);
+            // 
+            // TL_lblTulemus
+            // 
+            this.TL_lblTulemus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblTulemus.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblTulemus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblTulemus.Location = new System.Drawing.Point(351, 671);
+            this.TL_lblTulemus.Name = "TL_lblTulemus";
+            this.TL_lblTulemus.Size = new System.Drawing.Size(184, 31);
+            this.TL_lblTulemus.TabIndex = 3;
+            this.TL_lblTulemus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(216, 671);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tulemus:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblJukuTaring1
+            // 
+            this.TL_lblJukuTaring1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblJukuTaring1.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblJukuTaring1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblJukuTaring1.Location = new System.Drawing.Point(141, 426);
+            this.TL_lblJukuTaring1.Name = "TL_lblJukuTaring1";
+            this.TL_lblJukuTaring1.Size = new System.Drawing.Size(60, 60);
+            this.TL_lblJukuTaring1.TabIndex = 5;
+            this.TL_lblJukuTaring1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblJukuTaring2
+            // 
+            this.TL_lblJukuTaring2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblJukuTaring2.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblJukuTaring2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblJukuTaring2.Location = new System.Drawing.Point(265, 426);
+            this.TL_lblJukuTaring2.Name = "TL_lblJukuTaring2";
+            this.TL_lblJukuTaring2.Size = new System.Drawing.Size(60, 60);
+            this.TL_lblJukuTaring2.TabIndex = 6;
+            this.TL_lblJukuTaring2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblPeeterTaring2
+            // 
+            this.TL_lblPeeterTaring2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblPeeterTaring2.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblPeeterTaring2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblPeeterTaring2.Location = new System.Drawing.Point(684, 426);
+            this.TL_lblPeeterTaring2.Name = "TL_lblPeeterTaring2";
+            this.TL_lblPeeterTaring2.Size = new System.Drawing.Size(60, 60);
+            this.TL_lblPeeterTaring2.TabIndex = 7;
+            this.TL_lblPeeterTaring2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblPeeterTaring1
+            // 
+            this.TL_lblPeeterTaring1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblPeeterTaring1.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblPeeterTaring1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblPeeterTaring1.Location = new System.Drawing.Point(560, 426);
+            this.TL_lblPeeterTaring1.Name = "TL_lblPeeterTaring1";
+            this.TL_lblPeeterTaring1.Size = new System.Drawing.Size(60, 60);
+            this.TL_lblPeeterTaring1.TabIndex = 8;
+            this.TL_lblPeeterTaring1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblJukuTulemus
+            // 
+            this.TL_lblJukuTulemus.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblJukuTulemus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblJukuTulemus.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblJukuTulemus.ForeColor = System.Drawing.Color.Teal;
+            this.TL_lblJukuTulemus.Location = new System.Drawing.Point(141, 350);
+            this.TL_lblJukuTulemus.Name = "TL_lblJukuTulemus";
+            this.TL_lblJukuTulemus.Size = new System.Drawing.Size(184, 51);
+            this.TL_lblJukuTulemus.TabIndex = 9;
+            this.TL_lblJukuTulemus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblPeeterTulemus
+            // 
+            this.TL_lblPeeterTulemus.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblPeeterTulemus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TL_lblPeeterTulemus.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblPeeterTulemus.ForeColor = System.Drawing.Color.Teal;
+            this.TL_lblPeeterTulemus.Location = new System.Drawing.Point(560, 350);
+            this.TL_lblPeeterTulemus.Name = "TL_lblPeeterTulemus";
+            this.TL_lblPeeterTulemus.Size = new System.Drawing.Size(184, 51);
+            this.TL_lblPeeterTulemus.TabIndex = 10;
+            this.TL_lblPeeterTulemus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tl_lblJukuPunktid
+            // 
+            this.Tl_lblJukuPunktid.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Tl_lblJukuPunktid.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tl_lblJukuPunktid.ForeColor = System.Drawing.Color.Teal;
+            this.Tl_lblJukuPunktid.Location = new System.Drawing.Point(171, 309);
+            this.Tl_lblJukuPunktid.Name = "Tl_lblJukuPunktid";
+            this.Tl_lblJukuPunktid.Size = new System.Drawing.Size(129, 31);
+            this.Tl_lblJukuPunktid.TabIndex = 11;
+            this.Tl_lblJukuPunktid.Text = "Punktid";
+            this.Tl_lblJukuPunktid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tl_lblPeeterPunktid
+            // 
+            this.Tl_lblPeeterPunktid.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Tl_lblPeeterPunktid.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tl_lblPeeterPunktid.ForeColor = System.Drawing.Color.Teal;
+            this.Tl_lblPeeterPunktid.Location = new System.Drawing.Point(590, 309);
+            this.Tl_lblPeeterPunktid.Name = "Tl_lblPeeterPunktid";
+            this.Tl_lblPeeterPunktid.Size = new System.Drawing.Size(129, 31);
+            this.Tl_lblPeeterPunktid.TabIndex = 12;
+            this.Tl_lblPeeterPunktid.Text = "Punktid";
+            this.Tl_lblPeeterPunktid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblP1
+            // 
+            this.TL_lblP1.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblP1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblP1.Location = new System.Drawing.Point(122, 230);
+            this.TL_lblP1.Name = "TL_lblP1";
+            this.TL_lblP1.Size = new System.Drawing.Size(223, 79);
+            this.TL_lblP1.TabIndex = 13;
+            this.TL_lblP1.Text = "Esimene mängija Juku";
+            this.TL_lblP1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TL_lblP2
+            // 
+            this.TL_lblP2.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TL_lblP2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TL_lblP2.Location = new System.Drawing.Point(544, 230);
+            this.TL_lblP2.Name = "TL_lblP2";
+            this.TL_lblP2.Size = new System.Drawing.Size(223, 79);
+            this.TL_lblP2.TabIndex = 14;
+            this.TL_lblP2.Text = "Teine mängija Peeter";
+            this.TL_lblP2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Täring
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(882, 740);
+            this.Controls.Add(this.TL_lblP2);
+            this.Controls.Add(this.TL_lblP1);
+            this.Controls.Add(this.Tl_lblPeeterPunktid);
+            this.Controls.Add(this.Tl_lblJukuPunktid);
+            this.Controls.Add(this.TL_lblPeeterTulemus);
+            this.Controls.Add(this.TL_lblJukuTulemus);
+            this.Controls.Add(this.TL_lblPeeterTaring1);
+            this.Controls.Add(this.TL_lblPeeterTaring2);
+            this.Controls.Add(this.TL_lblJukuTaring2);
+            this.Controls.Add(this.TL_lblJukuTaring1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TL_lblTulemus);
+            this.Controls.Add(this.TL_btnP2);
+            this.Controls.Add(this.TL_btnP1);
+            this.Controls.Add(this.TL_startBtn);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Täring";
+            this.Text = "Täring";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TL_startBtn;
+        private System.Windows.Forms.Button TL_btnP1;
+        private System.Windows.Forms.Button TL_btnP2;
+        private System.Windows.Forms.Label TL_lblTulemus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TL_lblJukuTaring1;
+        private System.Windows.Forms.Label TL_lblJukuTaring2;
+        private System.Windows.Forms.Label TL_lblPeeterTaring2;
+        private System.Windows.Forms.Label TL_lblPeeterTaring1;
+        private System.Windows.Forms.Label TL_lblJukuTulemus;
+        private System.Windows.Forms.Label TL_lblPeeterTulemus;
+        private System.Windows.Forms.Label Tl_lblJukuPunktid;
+        private System.Windows.Forms.Label Tl_lblPeeterPunktid;
+        private System.Windows.Forms.Label TL_lblP1;
+        private System.Windows.Forms.Label TL_lblP2;
+    }
+}
+
---
+++ Liivamägi_OOP_KT3/Liivamägi_OOP_KT3/Täring.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Liivamägi_OOP_KT3
+{
+    
+    public partial class Täring : Form
+    {
+        int dice1;
+        int dice2;
+        int dice3;
+        int dice4;
+        int p1Result;
+        int p2Result;
+        Random random = new Random();
+
+        private void ClearAllFields()
+        {
+            foreach(Control control in Controls)
+            {
+                if(control is Label)
+                {
+                    Label label = (Label)control;
+                    if (label.Name.StartsWith("TL_lblJuku") || label.Name.StartsWith("TL_lblPeeter") || label.Name.StartsWith("TL_lblTulemus"))
+                    {
+                        label.Text = "";
+                    }
+                }
+            }
+        }
+        public Täring()
+        {
+            InitializeComponent();
+            TL_btnP1.Enabled = false;
+            TL_btnP2.Enabled = false;
+        }
+
+        private void TL_btnP1_Click(object sender, EventArgs e)
+        {
+            
+            dice1 = random.Next(1, 7);
+            dice2 = random.Next(1, 7);
+            TL_lblJukuTaring1.Text = Convert.ToString(dice1);
+            TL_lblJukuTaring2.Text = Convert.ToString(dice2);
+            p1Result = dice1 + dice2;
+            TL_lblJukuTulemus.Text = Convert.ToString(p1Result);
+            TL_btnP1.Enabled = false;
+            TL_btnP2.Enabled = true;
+        }
+
+        private void TL_btnP2_Click(object sender, EventArgs e)
+        {
+            dice3 = random.Next(1, 7);
+            dice4 = random.Next(1, 7);
+            TL_lblPeeterTaring1.Text = Convert.ToString(dice3);
+            TL_lblPeeterTaring2.Text = Convert.ToString(dice4);
+            p2Result = dice3 + dice4;
+            TL_lblPeeterTulemus.Text = Convert.ToString(p2Result);
+            TL_btnP2.Enabled = false;
+            
+            if(p2Result < p1Result) 
+            {
+                TL_lblTulemus.Text = "Võitis Juku!";
+            }else if(p2Result > p1Result)
+            {
+                TL_lblTulemus.Text = "Võitis Peeter!";
+            }
+            else
+            {
+                TL_lblTulemus.Text = "Viik!";
+            }
+            TL_startBtn.Enabled = true;
+            TL_startBtn.Focus();
+        }
+
+        private void TL_startBtn_Click(object sender, EventArgs e)
+        {
+            TL_btnP1.Enabled = true;
+            TL_startBtn.Enabled = false;
+            ClearAllFields();
+            
+        }
+    }
+}
